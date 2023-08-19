0a43081ef761f5d4a8d16515f949a94df4a1ae51
laslo-uri-ftn <laslo.uri@uns.ac.rs>
2023-08-19 20:02:33 +0200

    Initial Backend Setup
    Initialized backend project with Spring Boot Initializer, generating necessary files including the pom.xml and CriticomradeApplication.

---
+++ criticomrade/src/main/java/com/ftn/laslouri/criticomrade/CriticomradeApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.laslouri.criticomrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticomradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticomradeApplication.class, args);
+	}
+
+}
---
+++ criticomrade/src/test/java/com/ftn/laslouri/criticomrade/CriticomradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.laslouri.criticomrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriticomradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
